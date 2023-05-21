3d2018a209c21f313f9a77ed2188c37cf76e35bd
OlegKhiretdinov <oleg@oleg.win>
2023-05-21 13:59:28 +0300

    refactor: исправлены ошибки линтера

--- brain_games/layout/progression_layout.py
+++ brain_games/layout/progression_layout.py
@@ -5,7 +5,7 @@ def progression_layout():
     number = randint(1, 20)
     step = randint(1, 5)
     length = randint(5, 10)
-    hidden_element_index = randint(0, length-1)
+    hidden_element_index = randint(0, length - 1)  
     index = 0
     question = ""
 

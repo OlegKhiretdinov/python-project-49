7b405c05620c29cbd6b3014030fe12f7503be080
OlegKhiretdinov <oleg@oleg.win>
2023-05-24 22:13:23 +0300

    refactor опредление простого числа вынесено в функцию

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,15 +5,17 @@ RULES_DESCRIPTION = ('Answer "yes" if given number is prime. \
 Otherwise answer "no".')
 
 
-def get_round_data():
-    number = randint(2, 100)
-    correct_answer = "yes"
+def is_prime(number):
     i = 2
-
     while i < number:
         if number % i == 0:
-            correct_answer = "no"
-            break
+            return false
         i += 1
+    return true
+
+
+def get_round_data():
+    number = randint(2, 100)
+    correct_answer = "yes" if is_prime(number) else "no"
 
     return (correct_answer, number)

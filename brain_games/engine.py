55af8b0e55320dc96a72e1f2eb3f7a7884b91539
OlegKhiretdinov <oleg@oleg.win>
2023-05-24 21:21:48 +0300

    refactor while заменён на for

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,25 +1,22 @@
 from brain_games.cli import welcome_user, get_answer_for_question
 
 
-def engine(welcome_txt, round):
+ROUND_COUNT = 3
+
+
+def engine(game_description, round):
     name = welcome_user()
-    print(welcome_txt)
+    print(game_description)
 
-    win_count = 0
-    loose_count = 0
-
-    while win_count < 3 and loose_count < 1:
+    for _ in range(ROUND_COUNT):
         (correct_answer, question) = round()
         user_answer = get_answer_for_question(question)
         if user_answer == str(correct_answer):
             print("Correct!")
-            win_count += 1
         else:
             print(f'\'{user_answer}\' is wrong answer ;('
-                  f'. Correct answer was \'{correct_answer}\'.')
-            loose_count += 1
+                  f'. Correct answer was \'{correct_answer}\'.\n'
+                  f'Let\'s try again, {name}!')
+            return
 
-    if win_count == 3:
-        print(f'Congratulations, {name}!')
-    else:
-        print(f'Let\'s try again, {name}!')
+    print(f'Congratulations, {name}!')
